7f6469e1bd83abc56f23414ce92fe84f13bc9a88
Tommy Chen <tommy351@gmail.com>
2015-12-05 20:38:17 +0800

    highlight: lazy load language definitions

---
+++ scripts/build_highlight_alias.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var hljs = require('highlight.js');
+var languages = hljs.listLanguages();
+
+var dest = path.join(__dirname, '../build/highlight_alias.json');
+var result = {
+  languages: languages,
+  aliases: {}
+};
+
+languages.forEach(function(lang){
+  result.aliases[lang] = lang;
+
+  var def = require('highlight.js/lib/languages/' + lang)(hljs);
+  var aliases = def.aliases;
+
+  if (aliases){
+    aliases.forEach(function(alias){
+      result.aliases[alias] = lang;
+    });
+  }
+});
+
+fs.writeFile(dest, JSON.stringify(result), function(err){
+  if (err) return console.error(err);
+
+  console.log('Highlight alias build success');
+});
