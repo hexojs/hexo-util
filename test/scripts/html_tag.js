d05ba8ba95c8d3c83e97157da7cfdfef2825dd48
Xaviju <xaviju@gmail.com>
2018-07-31 17:07:20 +0200

    Allows empty attributes in html tag helper

--- test/scripts/html_tag.js
+++ test/scripts/html_tag.js
@@ -27,6 +27,34 @@ describe('htmlTag', function() {
     }, 'My blog').should.eql('<a href="http://zespia.tw">My blog</a>');
   });
 
+  it('tag + empty ALT attr', function() {
+    htmlTag('img', {
+      src: 'http://placekitten.com/200/300',
+      alt: ''
+    }).should.eql('<img src="http://placekitten.com/200/300" alt="">');
+  });
+
+  it('passing a zero as attribute', function() {
+    htmlTag('a', {
+      href: 'http://zespia.tw',
+      tabindex: 0
+    }, 'My blog').should.eql('<a href="http://zespia.tw" tabindex="0">My blog</a>');
+  });
+
+  it('passing a null alt attribute', function() {
+    htmlTag('a', {
+      href: 'http://zespia.tw',
+      alt: null
+    }, 'My blog').should.eql('<a href="http://zespia.tw">My blog</a>');
+  });
+
+  it('passing a undefined alt attribute', function() {
+    htmlTag('a', {
+      href: 'http://zespia.tw',
+      alt: undefined
+    }, 'My blog').should.eql('<a href="http://zespia.tw">My blog</a>');
+  });
+
   it('tag is required', function() {
     try {
       htmlTag();
