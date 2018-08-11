b15b51812b30b307edc4295c9140582351211c39
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2018-08-11 10:21:29 +0200

    Fix sublanguages highlight

--- test/scripts/highlight.js
+++ test/scripts/highlight.js
@@ -193,6 +193,18 @@ describe('highlight', function() {
     validateHtmlAsync(result, done);
   });
 
+  it('highlight sublanguages', function(done) {
+    var str = '<node><?php echo "foo"; ?></node>';
+    var result = highlight(str, {lang: 'xml'});
+    result.should.eql([
+      '<figure class="highlight xml"><table><tr>',
+      gutter(1, 1),
+      code('<span class="tag">&lt;<span class="name">node</span>&gt;</span><span class="php"><span class="meta">&lt;?php</span> <span class="keyword">echo</span> <span class="string">"foo"</span>; <span class="meta">?&gt;</span></span><span class="tag">&lt;/<span class="name">node</span>&gt;</span>', null),
+      end
+    ].join(''));
+    validateHtmlAsync(result, done);
+  });
+
   it('parse multi-line strings correctly', function(done) {
     var str = [
       'var string = `',
