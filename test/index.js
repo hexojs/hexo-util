ce25980c1549de216a721620ff927fd19107d389
segayuu <segayuu@gmail.com>
2019-06-10 22:34:05 +0900

    Add Color class from tagcloud hexo helper

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 describe('util', () => {
   require('./scripts/cache_stream');
   require('./scripts/camel_case_keys');
+  require('./scripts/color');
   require('./scripts/escape_diacritic');
   require('./scripts/escape_html');
   require('./scripts/escape_regexp');
