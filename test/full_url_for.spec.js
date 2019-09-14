e7f42754ed7f1c748257cb47e61348c40d3c92f4
SukkaW <isukkaw@gmail.com>
2019-09-14 21:58:37 +0800

    fix: minor changes
    Apply suggestions from code review by @curbengh
    
    - Remove useless code comment
    - Remove never used test configuration

--- test/full_url_for.spec.js
+++ test/full_url_for.spec.js
@@ -8,21 +8,18 @@ describe('full_url_for', () => {
   const fullUrlFor = require('../lib/full_url_for').bind(ctx);
 
   it('internal url - root directory', () => {
-    ctx.config.root = '/';
     ctx.config.url = 'https://example.com';
     fullUrlFor('index.html').should.eql(ctx.config.url + '/index.html');
     fullUrlFor('/').should.eql(ctx.config.url + '/');
   });
 
   it('internal url - subdirectory', () => {
-    ctx.config.root = '/blog/';
     ctx.config.url = 'https://example.com/blog';
     fullUrlFor('index.html').should.eql(ctx.config.url + '/index.html');
     fullUrlFor('/').should.eql(ctx.config.url + '/');
   });
 
   it('internal url - no duplicate slash', () => {
-    ctx.config.root = '/';
     ctx.config.url = 'https://example.com';
     fullUrlFor('/index.html').should.eql('https://example.com/index.html');
   });
@@ -37,7 +34,6 @@ describe('full_url_for', () => {
   });
 
   it('only hash', () => {
-    ctx.config.root = '/blog/';
     ctx.config.url = 'https://example.com/blog';
     fullUrlFor('#test').should.eql(ctx.config.url + '/#test');
   });
