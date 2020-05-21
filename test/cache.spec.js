a36d2a92aa1b3627f8139599bb8e82baa58031af
SukkaW <isukkaw@gmail.com>
2020-05-21 23:24:08 +0800

    feat/refactor(cache): use Map, cache#dump, cache#size

--- test/cache.spec.js
+++ test/cache.spec.js
@@ -11,6 +11,11 @@ describe('Cache', () => {
     cache.get('foo').should.eql(123);
   });
 
+  it('size', () => {
+    cache.set('foobar', 456);
+    cache.size().should.eql(2);
+  });
+
   it('has', () => {
     cache.has('foo').should.eql(true);
     cache.has('bar').should.eql(false);
@@ -28,6 +33,15 @@ describe('Cache', () => {
     cache.apply('baz', () => 456).should.eql(123);
   });
 
+  it('dump', () => {
+    cache.dump().should.eql({
+      'bar': 123,
+      'baz': 123,
+      'foo': 123,
+      'foobar': 456
+    });
+  });
+
   it('del', () => {
     cache.del('baz');
     cache.has('foo').should.eql(true);
@@ -39,6 +53,7 @@ describe('Cache', () => {
     cache.has('foo').should.eql(false);
     cache.has('bar').should.eql(false);
     cache.has('baz').should.eql(false);
+    cache.size().should.eql(0);
   });
 
   it('cache null', () => {
