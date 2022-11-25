62ba5040f18ae14bd3d04bff6cbd5c19614b29dc
Mimi <1119186082@qq.com>
2022-11-25 12:36:04 +0800

    permalink

--- lib/permalink.ts
+++ lib/permalink.ts
@@ -2,12 +2,18 @@ import escapeRegExp from './escape_regexp';
 
 const rParam = /:(\w*[^_\W])/g;
 
+interface Options {
+  segments?: {
+    [key: string]: RegExp | string;
+  }
+}
+
 class Permalink {
   rule: string;
   regex: RegExp;
   params: string[];
 
-  constructor(rule: string, options: { segments?: any; } = {}) {
+  constructor(rule: string, options: Options = {}) {
     if (!rule) { throw new TypeError('rule is required!'); }
     const segments = options.segments || {};
     const params = [];
