41137fd8ee17154dd1ecd526488a8bb79f4762fe
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-14 00:22:55 +0700

    fix: generic type
    - disable @typescript-eslint/no-explicit-any
    to be able treat `value` as any
    - fixed failed build caused by `Not all constituents of type 'T | function' are callable`

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export = class Cache<T> {
   cache: Map<string, T>;
 
@@ -21,7 +23,7 @@ export = class Cache<T> {
     this.cache.delete(id);
   }
 
-  apply(id: string, value: T): T {
+  apply(id: string, value: any): T {
     if (this.has(id)) return this.get(id);
 
     if (typeof value === 'function') value = value();
