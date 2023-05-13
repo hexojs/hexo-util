3abcc5d406a1391ee0580aca510ede6cc4f771fe
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-14 03:16:39 +0700

    chore: make cache mapper prop to private

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,57 +1,57 @@
 class CacheMapper<K, V> implements Map<K, V> {
-  cache: Map<K, V>;
+  private _innerMap: Map<K, V>;
   size: number;
 
   constructor() {
-    this.cache = new Map();
+    this._innerMap = new Map();
   }
   clear(): void {
-    this.cache.clear();
+    this._innerMap.clear();
   }
   delete(key: K): boolean {
-    throw this.cache.delete(key);
+    throw this._innerMap.delete(key);
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void {
-    return this.cache.forEach(callbackfn, thisArg);
+    return this._innerMap.forEach(callbackfn, thisArg);
   }
   entries(): IterableIterator<[K, V]> {
-    return this.cache.entries();
+    return this._innerMap.entries();
   }
   keys(): IterableIterator<K> {
-    return this.cache.keys();
+    return this._innerMap.keys();
   }
   values(): IterableIterator<V> {
-    return this.cache.values();
+    return this._innerMap.values();
   }
   [Symbol.iterator](): IterableIterator<[K, V]> {
-    return this.cache.entries();
+    return this._innerMap.entries();
   }
   [Symbol.toStringTag]: string;
 
   typeof() {
-    return typeof this.cache;
+    return typeof this._innerMap;
   }
 
   set(id: K, value: V) {
-    this.cache.set(id, value);
+    this._innerMap.set(id, value);
     // set cache size while set new value
-    this.size = this.cache.size;
+    this.size = this._innerMap.size;
     return this;
   }
 
   has(id: K) {
-    return this.cache.has(id);
+    return this._innerMap.has(id);
   }
 
   get(id: K) {
-    return this.cache.get(id);
+    return this._innerMap.get(id);
   }
 
   del(id: K) {
-    this.cache.delete(id);
+    this._innerMap.delete(id);
     // set cache size while delete value
-    this.size = this.cache.size;
+    this.size = this._innerMap.size;
   }
 
   apply(id: K, value: unknown) {
@@ -64,11 +64,11 @@ class CacheMapper<K, V> implements Map<K, V> {
   }
 
   flush() {
-    this.cache.clear();
+    this._innerMap.clear();
   }
 
   dump() {
-    return Object.fromEntries(this.cache);
+    return Object.fromEntries(this._innerMap);
   }
 }
 
