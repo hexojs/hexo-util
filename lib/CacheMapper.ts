1ed3491e6d7cd475958defd166f494eda61bca0a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-14 15:15:52 +0700

    docs(apply): add JSDoc to each overload types

--- lib/CacheMapper.ts
+++ lib/CacheMapper.ts
@@ -91,8 +91,26 @@ export class Cache<V> {
   size() {
     return this.cache.size;
   }
+
+  /**
+   * apply cache non-function
+   * @param key cache key string
+   * @param value cache value must same as constructor generic type
+   */
   apply(key: string, value: V): V;
+
+  /**
+   * apply cache with function
+   * @param key cache key string
+   * @param value cache value must same as constructor generic type
+   */
   apply(key: string, value: () => V): V;
+
+  /**
+   * apply cache
+   * @param key cache key string
+   * @param value cache value must same as constructor generic type
+   */
   apply(key: string, value: (() => V) | V) {
     return this.cache.apply(key, value);
   }
