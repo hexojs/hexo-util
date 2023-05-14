992a9fcaa920f415b89a3c906803b3c3e23c72ac
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-14 15:00:57 +0700

    chore: separate `Cache` then import to `cache.ts`

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,34 +1,3 @@
-import { CacheMapper } from './CacheMapper';
+import { Cache } from './CacheMapper';
 
-export = class Cache<V> {
-  cache: CacheMapper<string, V>;
-  constructor() {
-    this.cache = new CacheMapper<string, V>();
-  }
-  has(key: string) {
-    return this.cache.has(key);
-  }
-  get(key: string) {
-    return this.cache.get(key);
-  }
-  set(key: string, value: V) {
-    return this.cache.set(key, value);
-  }
-  dump() {
-    return Object.fromEntries(this.cache);
-  }
-  size() {
-    return this.cache.size;
-  }
-  apply(key: string, value: V): V;
-  apply(key: string, value: () => V): V;
-  apply(key: string, value: (() => V) | V) {
-    return this.cache.apply(key, value);
-  }
-  del(key: string) {
-    return this.cache.del(key);
-  }
-  flush() {
-    return this.cache.flush();
-  }
-};
+export = Cache;
