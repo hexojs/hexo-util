b791de3fb7106e0cdddebd99bb77224d59a44e43
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-07-31 21:28:05 +0700

    fix: update import path for highlight.js to include file extension

--- lib/highlight_shared.ts
+++ lib/highlight_shared.ts
@@ -1,5 +1,5 @@
 import type { HighlightResult, HLJSApi } from 'highlight.js';
-import _highlight from 'highlight.js/lib/core';
+import _highlight from 'highlight.js/lib/core.js';
 import stripIndent from 'strip-indent';
 import alias from './highlight_alias.js';
 import { InternalHighlightOptions } from './types.js';
